154dabba7d3e41328f21b9e3d0cbcfb05a9b4e51
move style to head <timmortal44@yahoo.com>
2023-03-23 13:08:56 +0100

    add the appropriate gap for mobile devices

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -12,11 +12,11 @@ const Tech = () => {
       <p className={styles.sectionSubText}>Toolbox</p>
       <h2 className={styles.sectionHeadText}>My Skills.</h2>
     </motion.div>
-    <div className="flex flew-row flex-wrap justify-center gap-10">
+    <div className="flex flew-row flex-wrap justify-center gap-24 md:gap-10">
       {technologies.map((technology) => (
         <div
           key={technology.name} 
-          className="w-28 h-28 "
+          className="w-28 h-28"
         >
           <BallCanvas icon={technology.icon} />
         </div>
